3dcf98d7d94bd5fe3d3b26c07183fb708e31898a
SENIOR\tobias.kiefer <tobias.kiefer@senior.com.br>
2025-03-17 16:34:40 -0300

    initial commit

---
+++ hotel-checkin-backend/src/main/java/org/example/hotelcheckinbackend/HotelCheckinBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.hotelcheckinbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCheckinBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelCheckinBackendApplication.class, args);
+    }
+
+}
---
+++ hotel-checkin-backend/src/test/java/org/example/hotelcheckinbackend/HotelCheckinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hotelcheckinbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelCheckinBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
